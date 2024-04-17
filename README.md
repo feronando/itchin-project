6faf346405afa527a916644e10337c5f74522fa1
Fernando Almeida <fernandogdalmeida@gmail.com>
2024-04-17 14:03:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# itchin-project
---
+++ src/test/java/br/imd/itchinproject/ItchinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.itchinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItchinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
